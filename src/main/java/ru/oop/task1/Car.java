6559f8bc29f81ea523c7341532571bec03d4e53f
Waranim <129619485+Waranim@users.noreply.github.com>
2024-10-18 12:22:10 +0500

    Complete 1 task

---
+++ src/main/java/ru/oop/task1/Car.java
@@ -0,0 +1,18 @@
+package ru.oop.task1;
+
+/**
+ * Машина
+ */
+public class Car {
+    /**
+     * Посадить человека за руль и задать изначальную позицию
+     */
+    public Car(Person person) {
+    }
+
+    /**
+     * Поехать до определённой позиции
+     */
+    public void go(Position destination) {
+    }
+}
